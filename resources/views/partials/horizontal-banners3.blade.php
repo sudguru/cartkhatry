01460ff1309e75e2d92ba37de73b521f03eb9f6c
Sudeep Gurung <sudeepgurung.sg@gmail.com>
2019-03-03 16:39:06 +0545

    wip

---
+++ resources/views/partials/horizontal-banners3.blade.php
@@ -0,0 +1,17 @@
+
+
+<div class="container">
+    <div class="row">
+        @foreach ($bannersH2->banners as $banner)
+        <div class="col-md-2">
+            {{-- <h3 class="subtitle">ABOUT US</h3> --}}
+            <div class="banner banner-image">
+                <a href="{{$banner->link}}" target="_blank">
+                    <img src="/storage/banners/{{$banner->banner}}"  alt="{{$banner->title}}">
+                </a>
+
+            </div><!-- End .banner -->
+        </div><!-- End .col-md-4 -->
+        @endforeach
+    </div><!-- End .row -->
+</div><!-- End .container -->
